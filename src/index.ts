ab54f2ede00ff28581155b0b4b9d976aad3024c9
Abdusa Alam <abdusa.alam@gmail.com>
2021-12-19 18:09:49 +0700

    Add email support property

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,24 @@ function getNestedObject(nestedObj: any, pathArr: string[]) {
 async function run() {
   const path: string = core.getInput('path');
   const prop: string[] = core.getInput('prop_path').split('.');
+  const property: string = core.getInput('property');
   try {
     const buffer = await readFileAsync(path);
     const json = JSON.parse(buffer.toString());
-    const nestedProp = getNestedObject(json, prop);
+
+    if (prop && property) {
+      throw new Error("input can't be multiple prop");
+    }
+
+    let nestedProp;
+    if (prop) {
+      nestedProp = getNestedObject(json, prop);
+    }
+
+    if (property) {
+      nestedProp = json[property];
+    }
+
     if (nestedProp) {
       core.setOutput('prop', nestedProp);
     } else {
